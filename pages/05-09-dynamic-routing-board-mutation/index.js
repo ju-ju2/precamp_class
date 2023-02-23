d1ae3402ea1eecff1c6a33dc739850b04d2eee18
한주연 <hjy2859@naver.com>
2023-02-23 17:27:34 +0900

    데이터 보내고 바로 페이지 받아오기

---
+++ pages/05-09-dynamic-routing-board-mutation/index.js
@@ -0,0 +1,71 @@
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const CREATE_BOARD = gql`
+  mutation typeSetting($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  const router = useRouter();
+
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const [나의함수] = useMutation(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    //routing이 늘 성공하는 것은 아니기 때문에 try 시도해봐! 라는 표현을 쓴다
+    try {
+      const result = await 나의함수({
+        variables: {
+          //variables가 $역할을 해주기때문에 한번 더 쓸 필요 없다
+          writer: writer, //$writer 랑 state 변수 writer랑 같아도 상관없다, 아예 다른 놈임
+          title: title,
+          contents: contents,
+        },
+      });
+      console.log(result);
+      alert(result.data.createBoard.message);
+      console.log(result.data.createBoard.number);
+      // router.push("/05-10-dynamic-routed-board-mutation/" + result.data.createBoard.number)
+      router.push(
+        `/05-10-dynamic-routed-board-mutation/${result.data.createBoard.number}`
+      ); //템플릿 리터럴로 바꿔주기
+    } catch (error) {
+      //try에 있는 내용을 시도하다가, 실패하면 아랫줄 모두 무시하고 catch가 실행됨
+      alert(error.message);
+    }
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      작성자: <input type="text" onChange={onChangeWriter}></input>
+      <br />
+      제목: <input type="text" onChange={onChangeTitle}></input>
+      <br />
+      내용: <input type="text" onChange={onChangeContents}></input>
+      <br />
+      <button onClick={onClickSubmit}>GRAPHQL-API(동기)요청하기</button>
+    </>
+  );
+}
---
+++ pages/05-10-dynamic-routed-board-mutation/[number]/index.js
@@ -0,0 +1,44 @@
+// next에서 [대괄호]를 다이나믹 라우팅으로 인식하기때문에 무조건 외워야함
+
+import { gql, useQuery } from "@apollo/client";
+import { useRouter } from "next/router";
+
+const FETCH_BOARD = gql`
+  query typeSetting($number: Int) {
+    fetchBoard(number: $number) {
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutedPage() {
+  const router = useRouter();
+  console.log(router);
+  console.log(router.query.number);
+  //routing페이지에서 입력한 번호대로 감
+
+  //useMutation은 내가 페이지에서 실행시켜야지만 실행되지만 useQuery는 페이지를 열자마자 실행된다.
+  // 그래서 데이터 값을 바로 넣어줘야한다
+  const { data } = useQuery(FETCH_BOARD, {
+    variables: { number: Number(router.query.number) },
+  });
+
+  console.log(data);
+
+  return (
+    <>
+      <div>{router.query.number}번 페이지로 이동이 완료되었습니다.</div>
+      {/* 위에서 데이터가 아직 받아오지 않았기때문에 아래처럼 표현하면 에러난다. 하지만 await를 쓰지않는다
+      데이터가 있으면 보여줘! 이기 떄문에 조건문을 추가한다 : 조건부 랜더링 */}
+      {/* <div>{data.fetchBoard.writer}</div> */}
+      <div>{data && data.fetchBoard.writer}</div>
+      {/* &&연산자 방식 */}
+      <div>{data ? data.fetchBoard.title : "데이터를 받아오는 중입니다"}</div>
+      {/* 삼항연산자 방식 */}
+      <div>{data?.fetchBoard.contents}</div>
+      {/* 옵셔널 체인 방식, 가장 많이 쓰이는 방식*/}
+    </>
+  );
+}
