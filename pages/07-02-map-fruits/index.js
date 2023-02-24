9d27bbed479f7f4ed3d97a6f973f7c4cdf1e656b
한주연 <hjy2859@naver.com>
2023-02-24 23:26:10 +0900

    map을 활용하여 배열안의 객체 꺼내오기

---
+++ pages/07-02-map-fruits/index.js
@@ -0,0 +1,85 @@
+//백엔드에서 받아온 데이터라고 가정(컴포넌트 위에 만든 이유 : 컴포넌트 리렌더링 되어도 다시 안만들어짐 )
+
+const FRUIT = [
+  {
+    number: 1,
+    title: "레드향",
+  },
+  {
+    number: 2,
+    title: "샤인머스켓",
+  },
+  {
+    number: 3,
+    title: "산청딸기",
+  },
+  {
+    number: 4,
+    title: "한라봉",
+  },
+  {
+    number: 5,
+    title: "사과",
+  },
+  {
+    number: 6,
+    title: "애플망고",
+  },
+  {
+    number: 7,
+    title: "딸기",
+  },
+  {
+    number: 8,
+    title: "천혜향",
+  },
+  {
+    number: 9,
+    title: "과일선물세트",
+  },
+  {
+    number: 10,
+    title: "귤",
+  },
+];
+
+export default function MapFruitsPage() {
+  //1번
+  const aaa = [
+    <div>1 레드향</div>,
+    <div>2 샤인머스켓</div>,
+    <div>3 산청딸기</div>,
+  ];
+  //리턴 {aaa}로 들어갈때는 []와 ,가 사라진다
+  //   return <>{aaa}</>;
+
+  //2번 : 1번이랑 결과 똑같다
+  const bbb = [
+    { number: 1, title: "레드향" },
+    { number: 2, title: "샤인머스켓" },
+    { number: 3, title: "산청딸기" },
+  ].map((el) => (
+    <div>
+      {el.number} {el.title}
+    </div>
+  ));
+
+  //   return <>{bbb}</>;
+
+  //3번
+  const ccc = FRUIT.map((el) => (
+    <div>
+      {el.number} {el.title}
+    </div>
+  ));
+
+  return (
+    <>
+      {FRUIT.map((el) => (
+        <div>
+          {el.number} {el.title}
+        </div>
+      ))}
+    </>
+  );
+}
