eaa2ac57ab404bdc27f1ebca005c5f9738a1d1a6
한주연 <hjy2859@naver.com>
2023-02-26 23:12:00 +0900

    기본 페이지 작성

---
+++ pages/08-01-board-new/index.js
@@ -0,0 +1,12 @@
+export default function BoardNewPage() {
+  return (
+    <>
+      <h1>등록페이지</h1>
+      제목: <input type="text"></input>
+      <br />
+      내용: <input type="text"></input>
+      <br />
+      <button>등록하기</button>
+    </>
+  );
+}
---
+++ pages/08-02-board-edit/index.js
@@ -0,0 +1,12 @@
+export default function BoardEditPage() {
+  return (
+    <>
+      <h1>수정페이지</h1>
+      제목: <input type="text"></input>
+      <br />
+      내용: <input type="text"></input>
+      <br />
+      <button>수정하기</button>
+    </>
+  );
+}
