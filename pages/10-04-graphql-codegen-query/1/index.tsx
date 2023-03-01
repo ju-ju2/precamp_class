2905a477cc9f37ac30b43531a1f46c8703c9a287
한주연 <hjy2859@naver.com>
2023-03-01 12:32:47 +0900

    Query에서 타입지정

--- pages/10-04-graphql-codegen-query/1/index.tsx
+++ pages/10-04-graphql-codegen-query/1/index.tsx
@@ -1,4 +1,8 @@
 import { gql, useQuery } from "@apollo/client";
+import {
+  IQuery,
+  IQueryFetchBoardArgs,
+} from "../../../src/commons/types/generated/types";
 
 const FETCH_BOARD = gql`
   query typeSetting($number: Int) {
@@ -13,7 +17,10 @@ const FETCH_BOARD = gql`
 export default function StaticRoutedPage() {
   //useMutation은 내가 페이지에서 실행시켜야지만 실행되지만 useQuery는 페이지를 열자마자 실행된다.
   // 그래서 데이터 값을 바로 넣어줘야한다
-  const { data } = useQuery(FETCH_BOARD, { variables: { number: 500 } });
+  const { data } = useQuery<Pick<IQuery, "fetchBoard">, IQueryFetchBoardArgs>(
+    FETCH_BOARD,
+    { variables: { number: 500 } }
+  );
 
   console.log(data);
 
@@ -23,11 +30,11 @@ export default function StaticRoutedPage() {
       {/* 위에서 데이터가 아직 받아오지 않았기때문에 아래처럼 표현하면 에러난다. 하지만 await를 쓰지않는다
       데이터가 있으면 보여줘! 이기 떄문에 조건문을 추가한다 : 조건부 랜더링 */}
       {/* <div>{data.fetchBoard.writer}</div> */}
-      <div>{data && data.fetchBoard.writer}</div>
+      <div>{data && data.fetchBoard?.writer}</div>
       {/* &&연산자 방식 */}
-      <div>{data ? data.fetchBoard.title : "데이터를 받아오는 중입니다"}</div>
+      <div>{data ? data.fetchBoard?.title : "데이터를 받아오는 중입니다"}</div>
       {/* 삼항연산자 방식 */}
-      <div>{data?.fetchBoard.contents}</div>
+      <div>{data?.fetchBoard?.contents}</div>
       {/* 옵셔널 체인 방식, 가장 많이 쓰이는 방식*/}
     </>
   );
