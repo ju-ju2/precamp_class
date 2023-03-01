31c518241e3cfddbd063dd7e5bb47858c0156cc4
한주연 <hjy2859@naver.com>
2023-03-01 12:29:31 +0900

    mutation에서 타입지정

---
+++ pages/10-03-graphql-codegen-mutation/index.tsx
@@ -0,0 +1,68 @@
+//args 는 arguments로 인자가 들어가는 것을 뜻한다
+// 아래의 값들이 지정하는 것이 아닌 인풋창에 입역되는 값으로 들어가는 것
+
+import { gql, useMutation } from "@apollo/client";
+import { useState } from "react";
+import {
+  IMutation,
+  IMutationCreateBoardArgs,
+} from "../../src/commons/types/generated/types";
+
+const CREATE_BOARD = gql`
+  mutation typeSetting($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+
+export default function GraphqlMutationPage() {
+  const [writer, setWriter] = useState<string>("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  // const [나의함수] = useMutation<RESULT타입, VARIABLES(인자)타입>(CREATE_BOARD);
+  const [나의함수] = useMutation<
+    Pick<IMutation, "createBoard">,
+    IMutationCreateBoardArgs
+  >(CREATE_BOARD);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables: {
+        //variables가 $역할을 해주기때문에 한번 더 쓸 필요 없다
+        writer: writer, //$writer 랑 state 변수 writer랑 같아도 상관없다, 아예 다른 놈임
+        title: title,
+        contents: contents,
+      },
+    });
+    console.log(result);
+    alert(result.data?.createBoard?.message);
+  };
+
+  const onChangeWriter = (event) => {
+    setWriter(event.target.value);
+  };
+
+  const onChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const onChangeContents = (event) => {
+    setContents(event.target.value);
+  };
+
+  return (
+    <>
+      작성자: <input type="text" onChange={onChangeWriter}></input>
+      <br />
+      제목: <input type="text" onChange={onChangeTitle}></input>
+      <br />
+      내용: <input type="text" onChange={onChangeContents}></input>
+      <br />
+      <button onClick={onClickSubmit}>GRAPHQL-API(동기)요청하기</button>
+    </>
+  );
+}
---
+++ pages/10-04-graphql-codegen-query/3/index.js
@@ -0,0 +1,34 @@
+import { gql, useQuery } from "@apollo/client";
+
+const FETCH_BOARD = gql`
+  query typeSetting($number: Int) {
+    fetchBoard(number: $number) {
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutedPage() {
+  //useMutation은 내가 페이지에서 실행시켜야지만 실행되지만 useQuery는 페이지를 열자마자 실행된다.
+  // 그래서 데이터 값을 바로 넣어줘야한다
+  const { data } = useQuery(FETCH_BOARD, { variables: { number: 238 } });
+
+  console.log(data);
+
+  return (
+    <>
+      <div>3번 페이지로 이동이 완료되었습니다.</div>
+      {/* 위에서 데이터가 아직 받아오지 않았기때문에 아래처럼 표현하면 에러난다. 하지만 await를 쓰지않는다
+      데이터가 있으면 보여줘! 이기 떄문에 조건문을 추가한다 : 조건부 랜더링 */}
+      {/* <div>{data.fetchBoard.writer}</div> */}
+      <div>{data && data.fetchBoard.writer}</div>
+      {/* &&연산자 방식 */}
+      <div>{data ? data.fetchBoard.title : "데이터를 받아오는 중입니다"}</div>
+      {/* 삼항연산자 방식 */}
+      <div>{data?.fetchBoard.contents}</div>
+      {/* 옵셔널 체인 방식, 가장 많이 쓰이는 방식*/}
+    </>
+  );
+}
