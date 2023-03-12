99c7cc1aa320daf43670706b043c9c8fb9be7c65
한주연 <hjy2859@naver.com>
2023-03-12 20:15:30 +0900

    _app.tsx 아폴로 세팅 분리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,14 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { AppProps } from "next/app";
+import ApolloSetting from "../src/components/commons/apollo";
 import Layout from "../src/components/commons/layout";
 // import "antd/dist/antd.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const client = new ApolloClient({
-    uri: "http://practice.codebootcamp.co.kr/graphql",
-    cache: new InMemoryCache(),
-  });
-
   return (
-    <ApolloProvider client={client}>
+    <ApolloSetting>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ApolloProvider>
+    </ApolloSetting>
   );
 }
---
+++ src/components/commons/apollo/index.tsx
@@ -0,0 +1,18 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+interface IApolloSettingProps {
+  children: JSX.Element;
+}
+
+export default function ApolloSetting(props: IApolloSettingProps) {
+  const client = new ApolloClient({
+    uri: "http://practice.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  // prettier-ignore
+  // 주석으로 prettier-ignore 해주면 한줄로 바뀌는걸 막아준다
+  return <ApolloProvider client={client}>
+    {props.children}
+    </ApolloProvider>;
+}
