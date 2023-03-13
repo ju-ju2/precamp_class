d63962108b9af51de38927dd16f6423c36c7c2df
한주연 <hjy2859@naver.com>
2023-03-13 19:08:40 +0900

    다음 페이지 구성(10개씩 넘어가기)

--- pages/14-02-pagenation-next/index.tsx
+++ pages/14-02-pagenation-next/index.tsx
@@ -1,6 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import styled from "@emotion/styled";
-import { MouseEvent } from "react";
+import { MouseEvent, useState } from "react";
 import {
   IQuery,
   IQueryFetchBoardsArgs,
@@ -27,6 +27,8 @@ const Column = styled.div`
 `;
 
 export default function StaticRoutedPage() {
+  const [startPage, setStartPage] = useState(1);
+
   const { data, refetch } = useQuery<
     Pick<IQuery, "fetchBoards">,
     IQueryFetchBoardsArgs
@@ -41,6 +43,17 @@ export default function StaticRoutedPage() {
     await refetch({ page: Number(event.currentTarget.id) });
     // void써도 상관없다. 아래 로직에 이패치 받은걸로 어떤걸 하면 무조건 await를 써줘야함
     // currentTarget: 현재 클릭한 현재 타겟 vs eventTarget: 이미지, 파일 등 여러곳에 들어가기 때문에 태그가 아닐수도있어서 무조건 아이디가 있음을 보장할 수 없다.
+  };
+
+  const onClickPrevPage = async () => {
+    setStartPage((prev) => prev - 10);
+    await refetch({ page: startPage - 10 });
+  };
+  const onClickNextPage = async () => {
+    setStartPage((prev) => prev + 10);
+    // 기존 값에 10을 더하는 로직
+    // setStartPage(startPage + 10) 해도 된다.
+    await refetch({ page: startPage + 10 });
   };
 
   return (
@@ -63,40 +76,17 @@ export default function StaticRoutedPage() {
         </Row>
       ))}
 
-      {/* 4. 3을 간단하게 표현하는 법   10개가 들어가는 배열을 만들고 그것을 1로 채운다는 뜻 */}
-
+      <span onClick={onClickPrevPage}>이전페이지</span>
       {new Array(10).fill(1).map((_, index) => (
-        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
-          {index + 1}
+        <span
+          key={index + startPage}
+          id={String(index + startPage)}
+          onClick={onClickPage}
+        >
+          {index + startPage}
         </span>
       ))}
-
-      {/* 3. el을 쓰지않고 index로 map적용하기,   el을 쓰지 않을때 _(언더바)로 표현하는 법 */}
-
-      {/* {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, index) => (
-        <span key={index + 1} id={String(index + 1)} onClick={onClickPage}>
-          {index + 1}
-        </span>
-      ))} */}
-
-      {/* 2. 배열 만들어서 map 적용하는 방법 */}
-
-      {/* {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((el) => (
-        <span key={el} id={String(el)} onClick={onClickPage}>
-          {el}
-        </span>
-      ))} */}
-
-      {/* 1. 가장 기본적인 방법 */}
-      {/* <span id="1" onClick={onClickPage}>
-        1
-      </span>
-      <span id="2" onClick={onClickPage}>
-        2
-      </span>
-      <span id="3" onClick={onClickPage}>
-        3
-      </span> */}
+      <span onClick={onClickNextPage}>다음페이지</span>
     </>
   );
 }
