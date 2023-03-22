a93dee82a18f2d6b4b873d80d79267ddbbf44bd8
한주연 <hjy2859@naver.com>
2023-03-22 22:41:30 +0900

    login User 뮤테이션에서 accessToken 받아오기/ global State에 accessToken 저장하기

--- src/commons/store/index.ts
+++ src/commons/store/index.ts
@@ -4,3 +4,8 @@ export const isEditState = atom({
   key: "isEditState", // state들 값의 키에 value가 저장됨
   default: false, // 초기값
 });
+
+export const accessTokenState = atom({
+  key: "accessTokenState",
+  default: "",
+});
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -5,6 +5,8 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/store";
 
 const GLOBAL_STATE = new InMemoryCache();
 // 페이지 전환(_app.tsx 리랜더)이 있더라도 기존에 있던 캐시는 이곳에 저장되기 때문에 캐시가 초기화 되지 않는다.
@@ -13,9 +15,12 @@ interface IApolloSettingProps {
   children: JSX.Element;
 }
 
+const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
 export default function ApolloSetting(props: IApolloSettingProps) {
   const uploadLink = createUploadLink({
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
   });
 
   const client = new ApolloClient({
