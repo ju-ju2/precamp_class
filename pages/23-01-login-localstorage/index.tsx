227b239fb02b0914a64ba2bf46b78067d4844a07
한주연 <hjy2859@naver.com>
2023-03-23 17:19:12 +0900

    next.js 프론트서버 존재/ 로컬 스토리지에 토큰 저장 후 새로고침시에도 로그인 정보 받기(임시)

---
+++ pages/23-01-login-localstorage/index.tsx
@@ -0,0 +1,71 @@
+import { gql, useMutation } from "@apollo/client";
+import { Modal } from "antd";
+import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../src/commons/store";
+import {
+  IMutation,
+  IMutationLoginUserArgs,
+} from "../../src/commons/types/generated/types";
+
+const LOGIN_USER = gql`
+  mutation typesetting($password: String!, $email: String!) {
+    loginUser(password: $password, email: $email) {
+      accessToken
+    }
+  }
+`;
+
+export default function LoginPage() {
+  const router = useRouter();
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUser] = useMutation<
+    Pick<IMutation, "loginUser">,
+    IMutationLoginUserArgs
+  >(LOGIN_USER);
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const onClickLogin = async () => {
+    try {
+      // 1. 로그인 해서 accessToken 받아오기
+      const result = await loginUser({
+        variables: {
+          email,
+          password,
+        },
+      });
+      const accessToken = result.data?.loginUser.accessToken;
+      console.log(accessToken);
+
+      // 2. accessToken을 global state에 저장하기  // 스코프체인 특성으로 바로 위의 accessToken을 가져옴
+      if (!accessToken) {
+        Modal.error({ content: "로그인에 실패했습니다. 다시 시도해주세요." });
+        return;
+      }
+      setAccessToken(accessToken);
+      // localStorage.setItem("qqq", "철수"); // qqq라는 키에 철수값이 담긴다
+      localStorage.setItem("accessToken", accessToken); // 임시사용(나중에 지울 예정)
+
+      // 3. 로그인 성공 페이지로 넘어가기
+      void router.push("/23-02-login-localstorage-success");
+    } catch (error) {
+      if (error instanceof Error) Modal.error({ content: error.message });
+    }
+  };
+  return (
+    <>
+      이메일: <input onChange={onChangeEmail} type="text" />
+      비밀번호: <input onChange={onChangePassword} type="password" />
+      <button onClick={onClickLogin}>로그인하기</button>
+    </>
+  );
+}
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -5,6 +5,7 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/store";
 
@@ -17,6 +18,34 @@ interface IApolloSettingProps {
 
 export default function ApolloSetting(props: IApolloSettingProps) {
   const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+  // 1. 프리렌더링(next.js에서는 브라우저에서 랜더링 되기 전에 프론트엔드 서버에서 한번 먼저 랜더링) 예제= process.browser방법
+  if (process.browser) {
+    // console.log("지금은 브라우저다");
+    // window.alert("안녕");
+    // const result = localStorage.getItem("accessToken");
+    // console.log(result);
+    // if (result) setAccessToken(result);
+  } else {
+    // console.log("지금은 프론트엔드 서버다");
+    // const result = localStorage.getItem("accessToken");
+    // console.log(result);
+    // if (result) setAccessToken(result);
+  }
+
+  // 2. 프리랜더링 예제 - typeof window 방법
+  if (typeof window !== "undefined") {
+    // console.log("지금은 브라우저다");
+  } else {
+    // console.log("지금은 프론트엔드 서버다");
+  }
+
+  // 3. 프리랜더링 무시 - useEffect
+  useEffect(() => {
+    console.log("지금은 브라우저다");
+    const result = localStorage.getItem("accessToken");
+    console.log(result);
+    if (result) setAccessToken(result);
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backendonline.codebootcamp.co.kr/graphql",
