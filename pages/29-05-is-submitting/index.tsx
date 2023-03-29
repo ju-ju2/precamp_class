e8e148a68e4abbe4a2a26b77a020e3582b29646b
한주연 <hjy2859@naver.com>
2023-03-29 14:20:21 +0900

    async await에서 setState가 중복적용되는 이유

--- pages/29-05-is-submitting/index.tsx
+++ pages/29-05-is-submitting/index.tsx
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export default function IsSubmitPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  //   const {formState} = useForm()
+  //   formState.isSubmitting() 도 가능
+
   const [myData, setMyData] = useState<any>();
   const onClickSubmit = async () => {
     setIsSubmitting(true);
@@ -20,3 +25,9 @@ export default function IsSubmitPage() {
     </>
   );
 }
+
+// 원래 setState는 함수안에 중복되어 실행되면 마지막 동작만 최종적으로 실행되는데 여기서는 둘 다 먹혔다.
+
+// onCLickSubmit이 실행되고 await를 만나면서 마이크로 태스크 큐에 들어간다
+// 들어가면서 setIsSubmitting(true); 에 한 흐름을 끊고 가기 때문에
+// setIsSubmitting(true)/ setIsSubmitting(false)가 둘다 가능했던것
