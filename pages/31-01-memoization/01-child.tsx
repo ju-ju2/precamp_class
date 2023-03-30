15d03ff97bdcbfcc8f50385beab3fac0b7352af4
한주연 <hjy2859@naver.com>
2023-03-30 21:55:08 +0900

    자식 컴포넌트 리랜더링을 막기위해 memo (React에서 제공) 로 감싸기

--- pages/31-01-memoization/01-child.tsx
+++ pages/31-01-memoization/01-child.tsx
@@ -1,4 +1,6 @@
-export default function MemoizationChildPage() {
+import { memo } from "react";
+
+function MemoizationChildPage() {
   console.log("자식이 랜더링 됩니다.");
 
   return (
@@ -9,3 +11,7 @@ export default function MemoizationChildPage() {
     </>
   );
 }
+
+// 부모 컴포넌트가 리랜더링될 때, 자식 컴포넌터가 불필요하게 리랜더링 되지 않도록
+// React에서 제공하는 memo에 넣어준다. (HOC 방식)
+export default memo(MemoizationChildPage);
